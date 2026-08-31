04dd9ce62265d83ccd4d1d2aa88312bc8e1fb059
elbahi03 <yassineelbahi44@gmail.com>
2026-08-31 18:20:32 +0200

    challange 2 : Variables & types

---
+++ Variables_types/ex2.js
@@ -0,0 +1,10 @@
+// Challenge 2 : Conversion de la température :
+
+var prompt = require('prompt-sync')();
+
+let tem = prompt("votre température en Celsius : ")
+console.log(" la transforme en Kelvin :");
+let k = tem + 273.15 ;
+console.log({k});
+
+
