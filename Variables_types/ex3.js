df8deecb18f0f77a8ed4047a89d6ceffbcaf78fd
elbahi03 <yassineelbahi44@gmail.com>
2026-08-31 18:25:39 +0200

    challange 3 : Variables & types

---
+++ Variables_types/ex3.js
@@ -0,0 +1,11 @@
+// Challenge 2 : Conversion de la distance :
+
+var prompt = require('prompt-sync')();
+
+let distance = prompt("la distance en kilomètres :")
+
+let yards = distance * 1093.61 ;
+
+console.log("la distance en yards");
+console.log({yards});
+
