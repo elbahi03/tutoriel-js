c7996977717656bb17f855b05ebb6c52916418d7
elbahi03 <yassineelbahi44@gmail.com>
2026-09-01 13:23:44 +0200

    challange 12 : Variables & types

---
+++ Variables_types/ex12.js
@@ -0,0 +1,16 @@
+// Challenge 12 : Nombre entier à quatre chiffres en ordre inverse :
+
+var prompt = require('prompt-sync')();
+
+let num = Number(prompt("valeur de nombre : "));
+let digital = 0 ;
+let inver = 0 ;
+while ( num != 0 ){
+    digital = num % 10 ;
+    inver = (inver * 10 ) + digital;
+    num = (num - digital) / 10 ;
+}
+
+console.log(inver);
+
+
