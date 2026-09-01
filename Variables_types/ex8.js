168f11fd065f30ef6a7469cb751efaaa9b55b12c
elbahi03 <yassineelbahi44@gmail.com>
2026-09-01 12:13:54 +0200

    challange 8 : Variables & types

---
+++ Variables_types/ex8.js
@@ -0,0 +1,13 @@
+// Challenge 8 : Calcul de la moyenne géométrique :
+
+var prompt = require('prompt-sync')();
+
+let num1 = Number(prompt("valeur 1 : "));
+let num2 = Number(prompt("valeur 2 : "));
+let num3 = Number(prompt("valeur 3 : "));
+
+let Moyenne = (num1 + num2 + num3)^(1/3);
+
+console.log("la moyenne géométrique de trois nombres :");
+console.log(Moyenne);
+
