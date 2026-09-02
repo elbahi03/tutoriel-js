9feb5c47a381b8dc7fa18bce3743c36673e7f89f
elbahi03 <yassineelbahi44@gmail.com>
2026-09-02 13:13:26 +0200

    challange 8 : Boucless

---
+++ Boucles/ex8.js
@@ -0,0 +1,17 @@
+// Challenge 8 : Affichage de la Suite de Fibonacci
+var prompt = require('prompt-sync')();
+
+let number = Number(prompt("nombre :"));
+let var1 = 0 ;
+let var2 = 1 ;
+let somme ;
+while (i <= number) {
+    console.log(n1);
+    somme = var1 + var2; 
+    var1 = var2;
+    var2 = somme; 
+    i++; 
+}
+
+console.log("F("+ number + ") = "+ somme);
+
