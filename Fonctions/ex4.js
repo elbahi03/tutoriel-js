eb4ed3a97d5295c10762ca1040cc99d0a44204e4
elbahi03 <yassineelbahi44@gmail.com>
2026-09-03 15:42:43 +0200

    challange  4 : Functions

---
+++ Fonctions/ex4.js
@@ -0,0 +1,24 @@
+// Challenge 4 : Fonction de Minimum
+
+// Challenge 3 : Fonction de Maximum : 
+
+function Min(a, b) {
+    let message ;
+    if (a > b) {
+        return b;
+    } else if (a < b) {
+        return a;
+    }else {
+        message = "sont egaux";
+        return message ;
+    }
+}
+
+var prompt = require('prompt-sync')();
+
+let num1 = Number(prompt("valeur de num 1 : "));
+let num2 = Number(prompt("valeur de num 2 : "));
+
+console.log("le min entre deux nombres : "+ Min(num1 , num2));
+
+
