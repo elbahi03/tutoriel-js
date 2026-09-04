3817c9a9db6bcfb1eea9a072ad4a9e0056944862
elbahi03 <yassineelbahi44@gmail.com>
2026-09-04 11:16:28 +0200

    challange  3 : Chaînes

---
+++ Chaînes/ex3.js
@@ -0,0 +1,10 @@
+// Challenge 3 : Concaténation de Chaînes
+
+var prompt = require('prompt-sync')();
+
+let text1 =prompt("entrer text  : ");
+let text2 =prompt("entrer text  : ");
+
+let text_resultat = text1 + " " + text2 ;
+
+console.log(" la chaîne résultante : "+ text_resultat)
