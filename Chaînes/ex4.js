dd172b65c05e2023f7e00e370f9c2ca8d5f6f611
elbahi03 <yassineelbahi44@gmail.com>
2026-09-04 11:19:56 +0200

    challange  4 : Chaînes

---
+++ Chaînes/ex4.js
@@ -0,0 +1,12 @@
+// Challenge 4 : Comparaison de Chaînes
+
+var prompt = require('prompt-sync')();
+
+let text1 =prompt("entrer text  : ");
+let text2 =prompt("entrer text  : ");
+
+if( text1 == text2 ){
+    console.log("egaux");
+}else {
+    console.log("not egaux");
+}
