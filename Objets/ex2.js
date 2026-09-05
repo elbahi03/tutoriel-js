29dcb51f123da7a773d94cae0a514c35e911a247
elbahi03 <yassineelbahi44@gmail.com>
2026-09-05 15:46:59 +0200

    challange  2 : Objet

---
+++ Objets/ex2.js
@@ -0,0 +1,12 @@
+// Challenge 2 : Objet avec Tableau
+
+let etudiant = {
+    full_name: "yassine el bahi",
+    age: "23",
+    note: [2, 5, 4, 8, 7]
+}
+
+console.log('les information d\'etudiant : ');
+console.log(etudiant.full_name);
+console.log(etudiant.age);
+console.table(etudiant.note);
