ff591ec1d9dfa338fd100692b35efe75d2556078
elbahi03 <yassineelbahi44@gmail.com>
2026-09-05 12:01:32 +0200

    challange  12 : Tableaux

---
+++ Tableaux/ex12.js
@@ -0,0 +1,16 @@
+// Challenge 12 : Afficher les Éléments Paire
+
+var prompt = require('prompt-sync')();
+
+let num = Number(prompt("number of tableu : "));
+let tableau = [];
+
+for (let i = 0; i < num; i++) {
+    tableau[i] = Number(prompt("tableu  " + i + ": "));
+}
+
+for (let i = 0; i < num; i++) {
+    if ( tableau[i] % 2 == 0 ){
+        console.log(tableau[i])
+    }
+}
