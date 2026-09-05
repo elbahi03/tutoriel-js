aa921cdb2e030e6bbe3fc9384092978443976108
elbahi03 <yassineelbahi44@gmail.com>
2026-09-05 17:29:47 +0200

    challange  4 : Objet

---
+++ Objets/ex4.js
@@ -0,0 +1,11 @@
+// Challenge 4 : Modification des Propriétés d'un Objet
+
+let propriétés = {
+    x: 8,
+    y: 4
+}
+
+x = Number(prompt("value of x : ")); 
+y = Number(prompt("value of y : "));
+
+console.log({propriétés});
