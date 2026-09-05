40515fc34a4d3ed08848a2141cd4d683d54703f8
elbahi03 <yassineelbahi44@gmail.com>
2026-09-05 12:37:20 +0200

    challange  7 : chaine

--- Chaînes/ex7.js
+++ Chaînes/ex7.js
@@ -3,18 +3,16 @@
 var prompt = require('prompt-sync')();
 
 let text = prompt("entrer text  : ");
-let new_text = ""
 
 for (let i = 0; i < text.length; i++) {
     let ACII = text.charCodeAt(i)
     if ((ACII >= 97) && (ACII <= 122)) {
         ACII -= 32;
         const Maj = String.fromCharCode(ACII);
-        // text[i] = text.replace(k, Maj);
-        new_text += Maj;
+        text = text.replace(text[i], Maj);
+        console.log(text[i], Maj )
     }else {
-        new_text += text[i];
+        continue ;
     }
 }
-text = new_text ;
 console.log({ text });
