c8e1b1140cf82eecd9b90180b196b5bcc9a7dbf6
elbahi03 <yassineelbahi44@gmail.com>
2026-09-10 12:14:51 +0200

    mison 4

--- mison_sutuation/mison4.js
+++ mison_sutuation/mison4.js
@@ -1,36 +1,53 @@
 let data = [
     {
         ville: "safi",
-        age: 18
+        age: 18,
+        note: [12, 17, 12]
     },
     {
         ville: "casablanca",
-        age: 20
+        age: 20,
+        note: [12, 17, 12]
     },
     {
         ville: "safi",
-        age: 15
+        age: 15,
+        note: [14, 17, 19]
     },
     {
         ville: "safi",
-        age: 25
+        age: 25,
+        note: [14, 17, 19]
     },
     {
         ville: "rabat",
-        age: 50
+        age: 50,
+        note: [14, 17, 19]
     },
     {
         ville: "safi",
-        age: 3
+        age: 3,
+        note: [14, 17, 19]
     },
 ]
 
 
+for (let i = 0; i <= data.length - 1; i++) {
+    data[i].moyenne = 0
+    for (let j = 0; j <= data[i].note.length - 1; j++) {
+        data[i].moyenne += data[i].note[j] ;
+    }
+    data[i].moyenne = data[i].moyenne / data[i].note.length;
+}
 
-for(i=0 ; i <= data.length -1 ; i++ ){
-    if(data[i].ville === "safi" && data[i].age >= 18){
-        console.log("ville :" + data[i].ville)
-        console.log("age :", data[i].age)
-        console.log("------------------")
+let max = 0;
+
+for (i = 0; i <= data.length - 1; i++) {
+    if (data[i].ville === "safi" && data[i].age >= 18){
+        if(max < data[i].moyenne){
+            max = data[i]
+        }
     }
 }
+
+console.log({max})
